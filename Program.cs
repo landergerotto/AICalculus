8e5a86055d15e892103b7599ad9a4c3631236055
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-02-19 16:36:45 -0300

    Bissection and FalsePosition

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AIContinuous;
+
+double myFuction(double x)
+{
+    // return x + 1;
+    // return Math.Sqrt(x) * x - 5*x + 10;
+    return Math.Sqrt(x) - Math.Cos(x);
+}
+
+double sol = Root.Bissction(myFuction, 0, 4);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine(sol);
---
+++ Solver/ContinuousSolver.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace AIContinuous;
+
+public class Root
+{
+    public static double Bissction(Func<double, double> function, double a, double b,
+                                    double tol = 1e-4, int maxIter = 1000)
+    {
+        var dt0 = DateTime.Now;
+        double c = 0;
+
+        for (int i = 0; i < maxIter; i++)
+        {
+            c = (a + b) / 2.0;
+            var fc = function(c);
+
+            if (Math.Abs(fc) < tol)
+                break;
+
+            // relative tolerance
+            if ((b - a) < tol * 2.0)
+                break;
+
+            if (fc * function(a) > 0)
+                a = c;
+            else
+                b = c;
+            
+
+
+        }
+        var dt1 = DateTime.Now;
+        var diff = dt1 - dt0;
+
+        Console.WriteLine("Executed in: " + diff.TotalMilliseconds + " Miliseconds");
+
+        return c;
+    }
+
+    public static double FalsePosition
+    (
+        Func<double, double> function, double a, double b,
+        double tol = 1e-4, int maxIter = 1000
+    )
+    {
+        var dt0 = DateTime.Now;
+        double c = 0;
+
+        for (int i = 0; i < maxIter; i++)
+        {
+            var fa = function(a);
+            var fb = function(b);
+            c = a - fa*(b - a)/(fb - fa);
+            
+            var fc = function(c);
+            if (Math.Abs(fc) < tol)
+                break;
+
+            if (b - a < tol * 2.0)
+                break;
+
+            if (fc * fa > 0)
+                a = c;
+            else
+                b = c;
+        }
+        var dt1 = DateTime.Now;
+        var diff = dt1 - dt0;
+
+        Console.WriteLine("Executed in: " + diff.TotalMilliseconds + " Miliseconds");
+
+        return c;
+    }
+}
