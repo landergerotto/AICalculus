27c298502862394823a1bfeef2b75afa03ab2109
Lander Furusho Gerotto <lander.gerotto@gmail.com>
2024-02-21 16:53:37 -0300

    Descendent Gradient

--- Optimize/Optimize.cs
+++ Optimize/Optimize.cs
@@ -15,11 +15,89 @@ public static class Optimize
     {
         Func<double, double> diffFunction = x 
             => Diff.Differentiate3P(func, x, h);
-            
+
         Func<double, double> diffSecondFunction = x 
             => Diff.Differentiate3P(diffFunction, x, h);
 
         return Root.Newton(diffFunction, diffSecondFunction, x0, tol, maxIter);
+    }
 
+    public static double DescendentGradient
+    (
+        Func<double, double> func, 
+        double x0,
+        double learningRate = 1e-2, // LR lr Lr lR
+        double tol = 1e-4
+    )
+    {
+        double xp = x0;
+        double diff = Diff.Differentiate3P(func, xp);
+
+        while (Math.Abs(diff) > tol)
+        {
+            diff = Diff.Differentiate3P(func, xp);
+            xp -= learningRate * diff;
+        }
+
+        return xp;
+    }
+
+    public static double Maximum
+    (
+        Func<double, double> func, 
+        double h = 1e-2,
+        double tol = 1e-4,
+        int maxIter = 10_000
+    )
+    {
+        Func<double, double> diffFunction = x 
+            => Diff.Differentiate3P(func, x, h);
+
+        Func<double, double> diffSecondFunction = x 
+            => Diff.Differentiate3P(diffFunction, x, h);
+
+        double[] allValues = new double[10_000];
+
+        double max = Double.MinValue;
+
+        for (int i = 0; i < 10_000; i++)
+        {
+            double guess = Random.Shared.NextInt64(-1_000_000, 1_000_000);
+            double value = Root.Newton(diffFunction, diffSecondFunction, guess, tol, maxIter);
+
+            if (value > max)
+                max = value;
+        }
+
+        return max;
+    }
+    public static double Minimum
+    (
+        Func<double, double> func, 
+        double h = 1e-2,
+        double tol = 1e-4,
+        int maxIter = 10_000
+    )
+    {
+        Func<double, double> diffFunction = x 
+            => Diff.Differentiate3P(func, x, h);
+
+        Func<double, double> diffSecondFunction = x 
+            => Diff.Differentiate3P(diffFunction, x, h);
+
+        double[] allValues = new double[10_000];
+
+        double min = Double.MaxValue;
+
+        for (int i = 0; i < 10_000; i++)
+        {
+            double guess = Random.Shared.NextInt64(-1_000_000, 1_000_000);
+            double value = Root.Newton(diffFunction, diffSecondFunction, guess, tol, maxIter);
+
+            if (value < min)
+                min = value;
+        }
+
+        return min;
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,18 +7,20 @@ double myFuction(double x)
     // return x + 1;
     // return Math.Sqrt(x) * x - 5*x + 10;
     return Math.Sqrt(x) - Math.Cos(x);
+    // return (x - 1) * (x - 1) - Math.Sin(x * x * x);
 }
 
 double NewtonFunction(double x)
 {
     // return x * x;
-    return (Math.Sqrt(Math.Abs(x)) - 5) * x + 10;
+    // return (Math.Sqrt(Math.Abs(x)) - 5) * x + 10;
+    return (x - 1) * (x - 1) + Math.Sin(x * x * x);
 }
 
 var date = DateTime.Now;
 
 date = DateTime.Now;
-double sold = Optimize.Newton(NewtonFunction, 1.0);
+double sold = Optimize.Minimum(NewtonFunction);
 var diff = DateTime.Now - date;
 
 Console.WriteLine("Executed in: " + diff.TotalMilliseconds + " Miliseconds");
